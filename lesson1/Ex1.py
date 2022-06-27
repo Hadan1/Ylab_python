43e5a3d8ccc1c1eb28dde9a2acb0bd67c99f2259
Hadani14 <103934527+Hadani14@users.noreply.github.com>
2022-06-27 15:19:01 +0300

    Почти

--- lesson1/Ex1.py
+++ lesson1/Ex1.py
@@ -7,13 +7,5 @@ def domain_name(url):
         n = m[:m.find('.')]
     else:
         n = m[:m.find('.')]
-<<<<<<< HEAD:lesson1/ex1.py
     return n
-=======
-    return n
-
-assert domain_name("http://google.com") == "google"
-assert domain_name("http://google.co.jp") == "google"
-assert domain_name("www.xakep.ru") == "xakep"
-assert domain_name("https://youtube.com") == "youtube"
->>>>>>> c3fcb1086f0985d0388626d6346ba7e25fd880dd:lesson1/Ex1.py
+    
