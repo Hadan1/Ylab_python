e737676aac5390a7a4cfc129ad12f6802da66db1
Hadani14 <103934527+Hadani14@users.noreply.github.com>
2022-06-28 03:31:03 +0300

    3ex

--- lesson1/Ex3.py
+++ lesson1/Ex3.py
@@ -1,7 +1,6 @@
 def zeros(n):
-    sum = 0
-    for i in range(5, n+1, 5):
-        while i % 5 == 0:
-            sum += 1
-            i = i // 5
-    return sum
+    result = 0
+    while n > 0:
+        n = n // 5
+        result = n + result
+    return result
