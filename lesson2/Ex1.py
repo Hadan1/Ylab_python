f288b66b17566a011190277e9d87549fd24fb5fb
Hadani14 <103934527+Hadani14@users.noreply.github.com>
2022-07-05 14:45:23 +0300

    last

--- lesson2/Ex1.py
+++ lesson2/Ex1.py
@@ -1,14 +1,14 @@
-points = [(2, 5), (5, 2), (6, 6), (8, 3)] #координаты - список кортежей, через запятую, указывается вручную
+points = [(2, 5), (5, 2), (6, 6), (8, 3), (9, 1), (2, 3), (4, 4), (5, 5), (6, 6)] #координаты - список кортежей, через запятую, указывается вручную
 start_point = (0, 2) #стартовая точка - кортеж, указывается вручную
 distance = 0
 point_neighbour = 0
 
-def find_length(point_1, point_2):
+def find_length(point_1, point_2): #вычисление расстояния
     return ((point_2[0] - point_1[0]) ** 2 + (point_2[1] - point_1[1]) ** 2) ** 0.5
 
 current_point = start_point
 
-while True:
+while True: #поиск пути через "Алгоритм ближайшего соседа"
     min_lengh = 0
     for i in points: #поиск ближайшей точки
         if find_length(current_point, i) < min_lengh or min_lengh == 0: 
--- lesson2/Ex2.py
+++ lesson2/Ex2.py
@@ -1,11 +1,12 @@
 from tkinter import *
+import tkinter.messagebox
 import random
 
 
 window = Tk()
 window.title("Обратные крестики-нолики")
 
-def press(cell_number):
+def press(cell_number): #нажатие на клетку
     token = 'X'
     turns[cell_number] = token
     buttons[cell_number].config(text=token, bg='white', state='disabled')
@@ -14,65 +15,66 @@ def press(cell_number):
     print(turns_left)
     losing_check(token)
 
-    computer_turn = random.choice(turns_left)
+    computer_turn = random.choice(turns_left) #ход компьютера
     token = 'O'
     turns[computer_turn] = token
     buttons[computer_turn].config(text=token, bg='green', state='disabled')
     turns_left.remove(computer_turn)
     if turns_left == []: #ничья
-        pass
+        tkinter.messagebox.showinfo('End game', 'Конец игры, ничья')
+    losing_check(token)
 
-def losing_check(token):
+def losing_check(token): #функция проверки конца игры
     main_count = -1
-    for i in turns:
+    for i in turns: #конец игры через горизонтальную линию слева-направо
         main_count += 1
         if turns[main_count] == token and (main_count % 10) <= 5:
             if turns[main_count] == turns[main_count+1] == turns[main_count+2] \
                 == turns[main_count+3] == turns[main_count+4]:
-                ending_color_line = [main_count, main_count+1, main_count+2, main_count+3, main_count+4]
-                print('END GAME')
+                ending_color_line = [buttons[main_count], buttons[main_count+1], buttons[main_count+2],
+                buttons[main_count+3], buttons[main_count+4]]
+                end_game(ending_color_line, token)
 
     main_count = -1
-    for i in turns:
+    for i in turns: #конец игры через вертикальную линию сверху-вниз
         main_count += 1
         if turns[main_count] == token and main_count < 60:
             if turns[main_count] == turns[main_count+10] == turns[main_count+20] \
                 == turns[main_count+30] == turns[main_count+40]:
-                ending_color_line = [main_count, main_count+10, main_count+20, main_count+30, main_count+40]
-                print('END GAME2')
+                ending_color_line = [buttons[main_count], buttons[main_count+10], buttons[main_count+20],
+                buttons[main_count+30], buttons[main_count+40]]
+                end_game(ending_color_line, token)
 
     main_count = -1
-    for i in turns:
+    for i in turns: #конец игры через диагональную снизу-вверх, слева-направо
         main_count += 1
         if turns[main_count] == token and main_count > 40 and (main_count % 10) <= 5:
             if turns[main_count] == turns[main_count-9] == turns[main_count-18] \
                 == turns[main_count-27] == turns[main_count-36]:
-                ending_color_line = [main_count, main_count-9, main_count-18, main_count-27, main_count-36]
-                print('END GAME3')
+                ending_color_line = [buttons[main_count], buttons[main_count-9], buttons[main_count-18], 
+                buttons[main_count-27], buttons[main_count-36]]
+                end_game(ending_color_line, token)
 
     main_count = -1
-    for i in turns:
+    for i in turns: #конец игры через диагональную сверху-вниз, слева-направо
         main_count += 1
         if turns[main_count] == token and main_count < 60 and (main_count % 10) <= 5:
             if turns[main_count] == turns[main_count+11] == turns[main_count+22] \
                 == turns[main_count+33] == turns[main_count+44]:
-                print('END GAME4')
-        
-
-            # if turns.index(i) < 60 and turns[turns.index(i)] == \
-            # turns[turns.index(i)+10] == turns[turns.index(i)+20] == turns[turns.index(i)+30] == turns[turns.index(i)+40]:
-            #     end_game()
+                ending_color_line = [buttons[main_count], buttons[main_count+11], buttons[main_count+22],
+                 buttons[main_count+33], buttons[main_count+44]]
+                end_game(ending_color_line, token)
             
-def end_game():
-    print('END GAME')
+def end_game(ending_color_line, token):
+    for i in buttons:
+        i.config(state='disabled')
+        if i in ending_color_line:
+            i.config(bg='red')
+    tkinter.messagebox.showinfo('Конец игры', 'Конец игры, ' + token + ' проиграли!')
 
-# up_diag_exception = [0, 1, 2, 3, 10, 11, 12, 20, 21, 30, 69, 78, 79, 87, 88, 89, 96, 97, 98, 99]
-# down_diag_exception = []
 turns = [None] * 100
 turns_left = list(range(100))
-
-buttons = [Button(command=lambda cell_number=i: press(cell_number), width=6, height=3, bg='grey') for i in range(100)]
-# width=3, height=1 font=('Arial', 28, 'bold')
+buttons = [Button(command=lambda cell_number=i: press(cell_number), width=3, height=1, font=('Arial', 28, 'bold'),bg='grey') for i in range(100)] 
 
 row = 1; col = 0
 for i in range(100):
@@ -81,4 +83,5 @@ for i in range(100):
     if col == 10:
         row += 1
         col = 0
+
 window.mainloop()
